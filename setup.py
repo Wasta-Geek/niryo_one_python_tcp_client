b450b972f4c46cd2924ccba819da374296359b9a
Wasta-Geek <corentin.ducatez@epitech.eu>
2019-08-02 14:15:54 +0200

    Tcp part + communication done (all commands, need further testing)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='niryo_one_tcp_client',
+      version='0.1',
+      description='A simple tcp client that manages the tcp communication with a Niryo One robot',
+      url='https://github.com/Wasta-Geek/niryo_one_python_tcp_client.git',
+      author='Corentin Ducatez',
+      author_email='c.ducatez@niryo.com',
+      license='GPLv3',
+      packages=['niryo_one_tcp_client'],
+      zip_safe=False)
